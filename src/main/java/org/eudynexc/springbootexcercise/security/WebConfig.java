a667121814b60772f544b6fbdb808a4f39486f38
ilias iosifidis <iosifidis90@gmail.com>
2026-05-03 18:42:24 +0300

    allow prod origins on CORS

--- src/main/java/org/eudynexc/springbootexcercise/security/WebConfig.java
+++ src/main/java/org/eudynexc/springbootexcercise/security/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry){
     registry.addMapping("/api/**")
-            .allowedOrigins("http://localhost:3000","https://sakila-frontend-production.up.railway.app/")
+            .allowedOrigins("http://localhost:3000","https://sakila-frontend-production.up.railway.app")
             .allowedMethods("GET","POST","PUT","DELETE")
             .allowCredentials(true);
   }
